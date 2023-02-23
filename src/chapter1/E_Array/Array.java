1431dedca8360a972f0fb1e3620a32ccac634599
jhe2426 <jhe2426@naver.com>
2023-02-23 12:46:52 +0900

    0223 수업(데이터 타입, 배열) 학습 내용 저장

--- src/chapter1/E_Array/Array.java
+++ src/chapter1/E_Array/Array.java
@@ -26,12 +26,58 @@ public class Array {
 		
 		//배열
 		//동일한 타입이면서 동일한 그룹의 변수들을 묶어서 관리해주는 형태
+		
 		// 선언 방법 : 데이터 타입[] 배열명;
 		//변수명 명명할 때 List를 붙이거나 s를 붙여 복수형으로 나타냄
 		int[] scores;
+		
 		//생성 방법 : 배열명 = new 데이터 타입[배열의 길이];
 		scores = new int[5];
-		System.out.println(scores);
+		System.out.println(scores);//참조형 변수이므로 메모리 주솟값이 들어가 있음
+		
+		//초기화 방법 : 배열명[인덱스 번호] = 데이터;
+		//인텍스는 0번 부터 시작
+		scores[0] = 90;
+		scores[1] = 100;
+		scores[2] = 95;
+		scores[3] = 75;
+		scores[4] = 80;
+		//오류, 예외가 발생하면 항상 메모하는 습관 가지기
+		
+		
+		//선언과 동시에 생성 및 초기화
+		
+		//1. 데이터 타입[] 배열명 = new 데이터타입[]{ 요소1, 요소2, 요소3, .....};
+		int[] scoreList = new int[] {90,100,95};
+		System.out.println(scoreList.length);
+		
+		//2. 데이터 타입[] 배열명 = {요소1, 요소2, .....};
+		char[] string = { 'h', 'e', 'l', 'l', 'o' };
+		System.out.println(string); //char의 배열은 문자열이라서 메모리 주솟값이 아닌 실제 데이터 값들이 출력이 됨
+		string[0] = 'k';
+		System.out.println(string[0]);
+		
+		
+		
+		//다차원 배열
+		//선언 방법 : 데이터 타입[][] 배열명;
+		//생성 방법 : 배열명 = new 데이터 타입[ 첫 번째 배열 길이 ][ 두 번째 배열 길이 ];
+		//선언과 동시에 초기화
+		//1. 데이터 타입[][] 배열명 = new 데이터 타입[][] { {요소11, ....}, { 요소21, ...}, ...... };
+		//2. 데이터 타입[][] 배열명 = { {요소11, ....}, {요소21, ....}, .... } ;
+		
+		//scoreList = new int[] {90,100,95};
+		int[] extraScoreList = scoreList; //배열은 참조형 데이터 이므로 참조형 변수에는 메모리 주솟값이 들어가 있으므로
+		//extraScoreList에 scoreList을 대입하면 두 개의 참조형 변수에는 같은 메모리 주솟값을 가지게 되는 것이다.
+		extraScoreList[0] = 70;
+		System.out.println(scoreList); //[I@515f550a
+		System.out.println(extraScoreList); //[I@515f550a
+		System.out.println(scoreList[0]); //70
+		System.out.println(extraScoreList[0]); //70
+		
+		
+		
+		
 		
 	}
 
---
+++ src/chapter1/F_Operator/Operator.java
@@ -0,0 +1,11 @@
+package chapter1.F_Operator;
+
+
+public class Operator {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
